0ff9533740c1f8ac3a39a40eab43e6d23bd1b872
Duong Thuy <58099641+dthuyfita@users.noreply.github.com>
2024-09-11 14:01:34 +0700

    Add files via upload

--- code_generator.py
+++ code_generator.py
@@ -3,7 +3,7 @@ import string
 
 prefix = input("Điền tiền tố cho code: \n")
 noc = int(input("Nhập số code cần sinh: \n"))
-code_len = int(input("Nhập độ dài code: \n"))
+code_len = int(input("Nhập độ dài code (đã bao gồm tiền tố): \n"))
 
 chars = string.ascii_uppercase+string.digits
 
@@ -12,7 +12,7 @@ for i in range(noc):
 
     for j in range(code_len-len(prefix)):
         code = code+random.choice(chars)
+        code = code.replace("0","IN",)
+        code = code.replace("O","11",)
 
     print(code)
-
-    
